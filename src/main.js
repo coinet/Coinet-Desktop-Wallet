d222599e753336cc435771a1740f2f4788943932
Ricardo Di Zio <11-11274@usb.ve>
2018-04-15 04:33:49 -0400

    coinet update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 
 nw.Window.open('app.html', {position: 'center', width: 1024, height: 800, min_width: 800, min_height: 600}, function(win) {
-	//win.showDevTools();
+	win.showDevTools();
 	
 	win.on('maximize', function () {
 		localStorage['windowState'] = "maximized";
